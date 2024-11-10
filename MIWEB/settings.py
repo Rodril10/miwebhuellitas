133c242a0732be2ebb388af227131f53c305aa60
Luis Rodriguez <luisenrique.rodvi@gmil.com>
2024-11-10 12:02:04 -0500

    validacion de erro test 1

--- MIWEB/settings.py
+++ MIWEB/settings.py
@@ -130,7 +130,8 @@ STATIC_URL = 'static/'
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 LOGIN_REDIRECT_URL = '/ingresar'
 
 
